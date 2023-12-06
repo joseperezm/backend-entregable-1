d0bff79227185eade5f163e716090f6a93a525ed
Jose Perez Merino <126683072+joseperezm@users.noreply.github.com>
2023-12-06 10:22:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 class ProductManager {
     constructor() {
       this.products = [];
-      this.productIdCounter = 1; // Contador para el ID autoincrementable
+      this.productIdCounter = 1;
     }
   
     addProduct(title, description, price, thumbnail, code, stock) {
-      // Validar que todos los campos sean obligatorios
       if (!title || !description || !price || !thumbnail || !code || !stock) {
         console.error('Todos los campos son obligatorios.');
         return;
       }
   
-      // Validar que el código no esté repetido
       const codeExists = this.products.some(product => product.code === code);
       if (codeExists) {
         console.error('El código del producto ya existe. Ingrese un código único.');
         return;
       }
   
-      // Agregar el producto con un ID autoincrementable
       const newProduct = {
         id: this.productIdCounter,
         title,
@@ -30,7 +27,7 @@ class ProductManager {
       };
   
       this.products.push(newProduct);
-      this.productIdCounter++; // Incrementar el contador de ID
+      this.productIdCounter++;
     }
   
     getProducts() {
